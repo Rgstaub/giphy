d7b7fe02cd8f973b20f822e1ae6c67c8bd6f7718
Rgstaub <rgstaub@gmail.com>
2017-07-03 14:58:35 -0400

    basic API calls are working and returning

--- js/script.js
+++ js/script.js
@@ -11,9 +11,28 @@ var apiKey = "bfeb7254f14c49bda91dfb078a370b0c";
 
 //make a button for each item in the array
 buttons.forEach(function(arr){
-	var button = $("<button/>").addClass("btn btn-default").attr("value", arr).text(arr);
+	var button = $("<button/>").addClass("btn btn-default movie").attr("value", arr).text(arr);
 	$("#buttonHome").append(button);
 })
 
+// The function to be called when a button is clicked. Use the button text to call 
+// giphy search API, adding "arrested development to the search terms"
+function getGifs() {
+	var picked = $(this).attr("value");
+	var search = picked.replace(" ", "+") + "+arrested+development";
+	console.log(search);
+	//build the query URL based on the text of the button clicked
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=10";
+	//make the API call
+	$.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+    	console.log(response);
+    	console.log(response.data[0].url);
+    	var gif = $("<img/>").attr("src", response.data[0].images.fixed_height.url);
+    	$("#gifPanel").append(gif);
+    })
+}
 
-
+$(document).on("click", ".movie", getGifs);
