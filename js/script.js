5671a4b703abac53c1ed1b98e1571eb5685dff2b
Rgstaub <rgstaub@gmail.com>
2017-07-04 22:23:05 -0400

    still in progress on adding buttons

--- js/script.js
+++ js/script.js
@@ -9,15 +9,30 @@ var apiKey = "bfeb7254f14c49bda91dfb078a370b0c";
 
 
 
+function drawInput() {
+
+var input = $("<form/>").attr("id", "input-form");
+var label = $("<label>").attr("for", "input-text").text("Create your own");
+var textBox = $("<input>").attr("type", "text").attr("id", "input-text");
+var button = $("<input>").attr("type", "submit").attr("id", "input-submit").attr("value", "Add");
+$("#searchPanel").append(input);
+input.append(label).append(textBox).append(button);
+
+}
 //make a button for each item in the array
-buttons.forEach(function(arr){
-	var button = $("<button/>").addClass("btn btn-default movie").attr("value", arr).text(arr);
-	$("#buttonHome").append(button);
-})
+function drawButtons() {
+	buttons.forEach(function(arr){
+		var button = $("<button/>").addClass("btn btn-default searchButton").attr("value", arr).text(arr);
+		$("#buttonHome").append(button);
+	})
+}
+
+
 
 // The function to be called when a button is clicked. Use the button text to call 
 // giphy search API, adding "arrested development to the search terms"
 function getGifs() {
+	$("#gifPanel").empty();
 	var picked = $(this).attr("value");
 	var search = picked.replace(" ", "+") + "+arrested+development";
 	console.log(search);
@@ -28,17 +43,30 @@ function getGifs() {
         url: queryURL,
         method: "GET"
     }).done(function(response) {
-    	// for each of the 10 returned results, create a container with the gif and rating within
+    	// for each of the 10 returned results, create a container with the gif (hidden), still image, and rating within
+    	console.log(response);
     	for (var i = 0; i < 10; i++) {
-    		console.log(response.data[i].url);
 	    	var gifBox = $("<div class='gifBox col-xs-12 col-sm-6 col-md-4 col-lg-3'/>");
-	    	var gif = $("<img>").attr("src", response.data[i].images.fixed_height.url);
+	    	var gif = $("<img>").attr("src", response.data[i].images.fixed_width.url).addClass("gif hidden");
+	    	var still = $("<img>").attr("src", response.data[i].images.fixed_width_still.url).addClass("gif");
 	    	var rating = $("<h4>").text("rating: " + response.data[i].rating);
 	    	$("#gifPanel").append(gifBox);
 	    	gifBox.append(gif);
+	    	gifBox.append(still);
 	    	gifBox.append(rating);
     	}
     })
 }
 
-$(document).on("click", ".movie", getGifs);
+// "play" and "pause" by toggling which one of the animated gif or still image is hidden
+function playPause() {
+	$(this).children("img").toggleClass("hidden");
+}
+
+drawButtons();
+
+drawInput();
+
+$(document).on("click", ".searchButton", getGifs);
+
+$(document).on("click", ".gifBox", playPause);
