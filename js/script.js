909312c9dad229c25b9008a2a7ba96af59983dc6
Rgstaub <rgstaub@gmail.com>
2017-07-05 16:21:02 -0400

    finished

--- js/script.js
+++ js/script.js
@@ -7,66 +7,89 @@
 var buttons = ["GOB", "Buster", "Michael", "George Sr.", "Lucille", "Lindsay", "George Michael", "Tobias", "Maebe", "Ann", "Steve Holt"];
 var apiKey = "bfeb7254f14c49bda91dfb078a370b0c";
 
+// Create the HTML elements that will provide structure for the page contents
+var container = $("<div class='container-fluid'><div class='row'></div></div>");
+$("body").prepend(container);
+var logoPanel = $("<div id='logoPanel' class='col-xs-12 col-md-6'>");
+logoPanel.html("<img src='images/logo.png' alt='arrested development logo'>");
+var searchPanel = $("<div id='searchPanel' class='col-xs-12 col-md-6'>");
+var buttonHome = $("<div id='buttonHome' class='col-xs-12 col-sm-3 col-md-2 col-lg-2'>");
+var gifPanel = $("<div id='gifPanel' class='col-xs-12 col-sm-9 col-md-10 col-lg-10'>");
+container.append(logoPanel).append(searchPanel).append(buttonHome).append(gifPanel);
 
-
-function drawInput() {
-
+// Create the HTML input form
 var input = $("<form/>").attr("id", "input-form");
-var label = $("<label>").attr("for", "input-text").text("Create your own");
-var textBox = $("<input>").attr("type", "text").attr("id", "input-text");
-var button = $("<input>").attr("type", "submit").attr("id", "input-submit").attr("value", "Add");
+var label = $("<label>").attr({for:"input-text", id:"input-label"}).text("Add your own ");
+var textBox = $("<input>").attr({type:"text", id:"input-text"});
+var button = $("<input>").attr({type:"submit", id:"input-submit", value:"Add"});
 $("#searchPanel").append(input);
 input.append(label).append(textBox).append(button);
 
-}
-//make a button for each item in the array
-function drawButtons() {
-	buttons.forEach(function(arr){
-		var button = $("<button/>").addClass("btn btn-default searchButton").attr("value", arr).text(arr);
-		$("#buttonHome").append(button);
-	})
-}
+$(document).ready(function() {
 
+	// Clear any existing buttons and make a button for each item in the array
+	function drawButtons() {
+		$("#buttonHome").empty();
+		buttons.forEach(function(arr){
+			var button = $("<button/>").addClass("btn btn-default searchButton").attr("value", arr).text(arr);
+			$("#buttonHome").append(button);
+		})
+	}
 
+	// get the text from the input field, add it to the button array, and redraw the buttons
+	function addButton() {
+		var input = $("#input-text").val().trim();
+		buttons.push(input);
+		drawButtons();
+		$("#input-text").val("");
+	}
 
-// The function to be called when a button is clicked. Use the button text to call 
-// giphy search API, adding "arrested development to the search terms"
-function getGifs() {
-	$("#gifPanel").empty();
-	var picked = $(this).attr("value");
-	var search = picked.replace(" ", "+") + "+arrested+development";
-	console.log(search);
-	//build the query URL based on the text of the button clicked
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=10";
-	//make the API call
-	$.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response) {
-    	// for each of the 10 returned results, create a container with the gif (hidden), still image, and rating within
-    	console.log(response);
-    	for (var i = 0; i < 10; i++) {
-	    	var gifBox = $("<div class='gifBox col-xs-12 col-sm-6 col-md-4 col-lg-3'/>");
-	    	var gif = $("<img>").attr("src", response.data[i].images.fixed_width.url).addClass("gif hidden");
-	    	var still = $("<img>").attr("src", response.data[i].images.fixed_width_still.url).addClass("gif");
-	    	var rating = $("<h4>").text("rating: " + response.data[i].rating);
-	    	$("#gifPanel").append(gifBox);
-	    	gifBox.append(gif);
-	    	gifBox.append(still);
-	    	gifBox.append(rating);
-    	}
-    })
-}
+	// The function to be called when a button is clicked. Use the button text to call 
+	// giphy search API, adding "arrested development to the search terms"
+	function getGifs() {
+		$("#gifPanel").empty();
+		var picked = $(this).attr("value");
+		var search = picked.replace(" ", "+") + "+arrested+development";
+		console.log(search);
+		//build the query URL based on the text of the button clicked
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=10";
+		//make the API call
+		$.ajax({
+	        url: queryURL,
+	        method: "GET"
+	    }).done(function(response) {
+	    	// for each of the 10 returned results, create a container with the gif (hidden), still image, and rating within
+	    	console.log(response);
+	    	for (var i = 0; i < 10; i++) {
+		    	var gifBox = $("<div class='gifBox col-xs-12 col-sm-6 col-md-4 col-lg-3'/>");
+		    	var gif = $("<img>").attr("src", response.data[i].images.fixed_width.url).addClass("gif hidden");
+		    	var still = $("<img>").attr("src", response.data[i].images.fixed_width_still.url).addClass("gif");
+		    	var rating = $("<h4>").text("rating: " + response.data[i].rating);
+		    	$("#gifPanel").append(gifBox);
+		    	gifBox.append(gif);
+		    	gifBox.append(still);
+		    	gifBox.append(rating);
+	    	}
+	    })
+	}
 
-// "play" and "pause" by toggling which one of the animated gif or still image is hidden
-function playPause() {
-	$(this).children("img").toggleClass("hidden");
-}
+	// "play" and "pause" by toggling which one of the animated gif or still image is hidden
+	function playPause() {
+		$(this).children("img").toggleClass("hidden");
+	}
 
-drawButtons();
+	// create the default buttons on load
+	drawButtons();
 
-drawInput();
+	// When a button is clicked, make an API call based on the value of the button and display the results
+	$(document).on("click", ".searchButton", getGifs);
 
-$(document).on("click", ".searchButton", getGifs);
+	// When a gif is clicked, "play" or "pause" the gif
+	$(document).on("click", ".gifBox", playPause);
 
-$(document).on("click", ".gifBox", playPause);
+	// When the input submit button is clicked, add a button based on the inpput text field
+	$(document).on("click", "#input-submit", function(event) {
+		event.preventDefault();
+		addButton();
+	});
+})
